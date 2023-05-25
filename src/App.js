6c7d4f0567d7211d8686abe91f91b019c8df6194
bedrovich <bedrovich2@gmail.com>
2023-05-26 03:13:43 +0400

    header

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import {
     Routes
 } from "react-router-dom";
 import { HomePage, UserPage } from "./pages";
+import Header from "./components/layout/Header";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/about"/>
-                    <Route path="/user/:id" element={ <UserPage/> }/>
-                    <Route exact path="/" element={ <HomePage/> }/>
-                </Routes>
-
+                <Header/>
+                <div className='body'>
+                    <Routes>
+                        <Route path="/about"/>
+                        <Route path="/user/:id" element={ <UserPage/> }/>
+                        <Route exact path="/" element={ <HomePage/> }/>
+                    </Routes>
+                </div>
             </BrowserRouter>
         </div>
     );
---
+++ src/components/layout/Header.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Button, Offcanvas } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Header() {
+    const [ show, setShow ] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const toggleShow = () => setShow((s) => !s);
+    const handleLinkClick = () => {
+        handleClose();
+        window.scrollTo(0, 0);
+    }
+
+    return (
+        <div className='header'>
+            <Button
+                variant="link"
+                onClick={toggleShow}
+            >
+                <img
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png'
+                    alt={ 'burger' }
+                    width='40'
+                />
+            </Button>
+
+            <Offcanvas
+                show={ show }
+                onHide={handleClose}
+                placement={ "start" }
+            >
+                <Offcanvas.Body>
+                    <div className='header-links'>
+                        <Link to='/' onClick={handleLinkClick}>
+                            <span>Список постов</span>
+                        </Link>
+                        <Link to='/about' onClick={handleLinkClick}>
+                            <span>Обо мне</span>
+                        </Link>
+                    </div>
+                </Offcanvas.Body>
+
+
+            </Offcanvas>
+        </div>
+    );
+}
+
+export default Header;
